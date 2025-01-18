a7bf491105e954b62ea12db165bde5a91a7ea971
nick <ntegan@duck.com>
2025-01-18 16:11:15 -0500

    mytune

--- opendbc/car/toyota/carcontroller.py
+++ opendbc/car/toyota/carcontroller.py
@@ -43,8 +43,7 @@ def get_long_tune(CP, params):
     kiV = [0.25]
 
   else:
-    kiBP = [0., 5., 35.]
-    kiV = [3.6, 2.4, 1.5]
+    kiV = [1.]
 
   return PIDController(0.0, (kiBP, kiV), k_f=1.0,
                        pos_limit=params.ACCEL_MAX, neg_limit=params.ACCEL_MIN,
--- opendbc/car/toyota/interface.py
+++ opendbc/car/toyota/interface.py
@@ -35,9 +35,9 @@ class CarInterface(CarInterfaceBase):
       ret.steerActuatorDelay = 0.18
       ret.steerLimitTimer = 0.8
     else:
-      CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
+      CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning, steering_angle_deadzone_deg=0.2)
 
-      ret.steerActuatorDelay = 0.12  # Default delay, Prius has larger delay
+      ret.steerActuatorDelay = 0.25  # Default delay, Prius has larger delay
       ret.steerLimitTimer = 0.4
 
     stop_and_go = candidate in TSS2_CAR
